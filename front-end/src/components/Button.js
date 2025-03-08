066d2fdc99018d3526fc1ab71224d2307a526917
swdomo <swd7169@nyu.edu>
2025-03-08 18:50:50 -0500

    add tailwind to button

--- front-end/src/components/Button.js
+++ front-end/src/components/Button.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-export function Button(props) {
-    const handleClick = () => {
-        alert(`Button clicked with title ${props.title}!`);
-    };
+const MyButton = ({children, onClick}) => {
+    const CLASSES = [
+        "font-bold",
+        "py-2",
+        "px-4",
+        "rounded",
+        "border-gray-400",
+        "border-2",
+        "bg-white",
+        "text-gray-600",
+        "background-gray-400",
+        "hover:bg-gray-400",
+        "hover:text-white",
+    ].join(" ");
 
     return (
-        <button onClick={handleClick} className="my-button">
-            {props.title}
+        <button onClick={onClick} class={CLASSES}>
+            {children}
         </button>
-    )
-}
+    );
+};
+
+export default MyButton;
