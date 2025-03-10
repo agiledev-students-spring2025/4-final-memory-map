786f6f167faf8505a28fe4f67fa98d6041acefd6
swdomo <swd7169@nyu.edu>
2025-03-10 00:14:56 -0400

    fix inline and class name

--- front-end/src/components/Button.js
+++ front-end/src/components/Button.js
@@ -1,25 +1,12 @@
 import React from "react";
 
-const MyButton = ({children, onClick}) => {
-    const CLASSES = [
-        "font-bold",
-        "py-2",
-        "px-4",
-        "rounded",
-        "border-gray-400",
-        "border-2",
-        "bg-white",
-        "text-gray-600",
-        "background-gray-400",
-        "hover:bg-gray-400",
-        "hover:text-white",
-    ].join(" ");
+const Button = ({children, onClick}) => {
 
     return (
-        <button onClick={onClick} class={CLASSES}>
+        <button onClick={onClick} className="font-bold py-2 px-4 rounded border-gray-400 border-2 bg-white text-gray-600 background-gray-400 hover:bg-gray-400 hover:text-white">
             {children}
         </button>
     );
 };
 
-export default MyButton;
+export default Button;
