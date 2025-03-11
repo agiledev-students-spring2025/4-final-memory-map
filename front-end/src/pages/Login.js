28ef2f89ac9e617f6e9fbefc00cda7ffa6372e86
abbyibarra <109445721+abbyibarra@users.noreply.github.com>
2025-03-11 18:48:26 -0400

    set up login page and edited routes

---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,13 @@
+const Login = () => {
+    return (
+        <div className="h-screen flex justify-center bg-white">
+            <div className="w-full max-w-md p-5">
+                <h2 className="text-left text-2xl font-bold text-black-500">Login</h2>
+                <form className="mt-2">
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login; 
