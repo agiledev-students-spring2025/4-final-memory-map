1f4286202c0d404a9ad16de4dd062eee04a015fc
kaiserarg <ultravenusk1@gmail.com>
2025-03-11 12:20:02 -0400

    mappin updates

--- front-end/src/pages/Feeds.js
+++ front-end/src/pages/Feeds.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+//import React, {useEffect, useState} from 'react';
 import LocationItem from '../components/LocationItem';
 import data from '../tempLocationData.json';
 
