d01f03b178394ca9772b5909d55709a05467d471
abbyibarra <109445721+abbyibarra@users.noreply.github.com>
2025-03-11 18:49:56 -0400

    added labels and input fields

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -4,6 +4,25 @@ const Login = () => {
             <div className="w-full max-w-md p-5">
                 <h2 className="text-left text-2xl font-bold text-black-500">Login</h2>
                 <form className="mt-2">
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+                            placeholder="Enter username"
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+                            placeholder="Enter password"
+                        />
+                    </div>
                 </form>
             </div>
         </div>
