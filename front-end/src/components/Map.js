0396b36f88b5f80b78f86086e7281c43dbab8848
kaiserarg <ultravenusk1@gmail.com>
2025-03-16 23:14:53 -0400

    update to api only

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,33 +1,30 @@
-//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import MapPin from './MapPin';
-import data from '../tempLocationData.json';
 
 const Map = () => {
-    /*
-    const [pinnedLocations, setPinnedLocations] = useState(null);
+    
+    const [pinnedLocations, setPinnedLocations] = useState([]);
 
     useEffect(() => {
         fetch('https://my.api.mockaroo.com/location.json', {
             headers: {
-                'X-API-Key': process.env.REACT_APP_MOCKAROO_KEY
+            'X-API-Key': process.env.REACT_APP_MOCKAROO_KEY
             }
         })
         .then(response => response.json())
-        .then(data => setPinnedLocations(data))
+        .then(data => {
+            setPinnedLocations(data);
+        })
         .catch(error => console.error('Error fetching location:', error));
     }, []);
-    */
-    const pinnedLocations = data;
 
-    let centerCoordinates = [40.7128, -74.0060];
-    if (pinnedLocations && pinnedLocations.length) {
-        centerCoordinates = [
-            pinnedLocations[0].pin_location_latitude,
-            pinnedLocations[0].pin_location_longitude,
-        ];
+    if (pinnedLocations.length === 0) {
+        return <div>Loading map...</div>;
     }
+    
+    const centerCoordinates = [pinnedLocations[0].pin_location_latitude, pinnedLocations[0].pin_location_longitude];
 
     return (
         <MapContainer
@@ -40,10 +37,9 @@ const Map = () => {
                 url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                 attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://carto.com/attributions">CARTO</a>'
             />
-            {pinnedLocations &&
-                pinnedLocations.map((pin, index) => (
-                    <MapPin key={index} pinData={pin} />
-                ))}
+            {pinnedLocations.map((pin, index) => (
+                <MapPin key={index} pinData={pin} />
+            ))}
         </MapContainer>
     );
 };
