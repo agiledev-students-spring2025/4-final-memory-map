8707950469d772a1a0aa011da3a61c73f8258836
Yinqi596 <yw6445@nyu.edu>
2025-04-06 17:29:39 -0400

    Update register.test.js

--- back-end/test/register.test.js
+++ back-end/test/register.test.js
@@ -5,7 +5,7 @@ import User from '../models/User.js';
 
 use(chaiHttp);
 
-describe('POST /api/user/register', () => {
+describe('POST /api/user/create_user', () => {
   const user = { email: 'test@example.com', password: '12345678' };
 
   beforeEach(async () => {
@@ -13,17 +13,17 @@ describe('POST /api/user/register', () => {
   });
 
   it('registers a new user', async () => {
-    const res = await chaiHttp.request(app).post('/api/user/register').send(user);
+    const res = await chaiHttp.request(app).post('/api/user/create_user').send(user);
     expect(res).to.have.status(201);
   });
 
   it('fails if email is missing', async () => {
-    const res = await chaiHttp.request(app).post('/api/user/register').send({ password: '12345678' });
+    const res = await chaiHttp.request(app).post('/api/user/create_user').send({ password: '12345678' });
     expect(res).to.have.status(400);
   });
 
   it('fails if password is missing', async () => {
-    const res = await chaiHttp.request(app).post('/api/user/register').send({ email: 'test@example.com' });
+    const res = await chaiHttp.request(app).post('/api/user/create_user').send({ email: 'test@example.com' });
     expect(res).to.have.status(400);
   });
 });
