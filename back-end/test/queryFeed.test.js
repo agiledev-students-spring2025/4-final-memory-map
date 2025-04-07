40334a83ff6f015821ee2411e781b0783b3009c3
swdomo <swd7169@nyu.edu>
2025-04-07 10:47:38 -0400

    add return 400 test

--- back-end/test/queryFeed.test.js
+++ back-end/test/queryFeed.test.js
@@ -20,13 +20,22 @@ describe('GET request to /query_feed route', function () {
   it('should respond with HTTP 200 and an object in the response body', async function () {
     
       chai.request(app)
-        .get('/query_friends?userId=3')
+        .get('/query_feed?userId=3')
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('object'); // Check that body is object
           expect(err).to.be.null;
           done();
         });
+    });
+
+    it('should respond with HTTP 400 if user not provided', async function () {
+        chai.request(app)
+            .get('/query_feed?')
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
     });
 
 });
