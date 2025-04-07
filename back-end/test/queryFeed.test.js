94578d1f8cb98b9fadef64a1cf2f4dc20dffa70b
swdomo <swd7169@nyu.edu>
2025-04-07 10:00:08 -0400

    query feed tests 0/2 pass --wip

---
+++ back-end/test/queryFeed.test.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import app from '../app.js';
+import route from '../routes/pin/queryFeed.js';
+
+import { use, expect } from 'chai';
+import chaiHttp from 'chai-http';
+
+const chai = use(chaiHttp);
+
+describe('GET request to /query_feed route', function () {
+    let app;
+
+    before(function () {
+        process.env.MOCKAROO_KEY = 'test-key';
+        app = express();
+        app.use(express.json());
+        app.use('/', route);
+      });
+
+  it('should respond with HTTP 200 and an object in the response body', async function () {
+    
+      chai.request(app)
+        .get('/query_friends?userId=3')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object'); // Check that body is object
+          expect(err).to.be.null;
+          done();
+        });
+    });
+
+});
--- back-end/test/queryFriends.test.js
+++ back-end/test/queryFriends.test.js
@@ -1,16 +1,16 @@
-import { use, expect, should, assert } from 'chai'
-import { default as chaiHttp, request } from 'chai-http'
+import { use, should, assert } from 'chai'
+import chaiHttp from 'chai-http';
 import app from '../app.js';
 
-use(chaiHttp);
+const chai = use(chaiHttp);
+const { expect } = chai;
 
 describe('GET request to /query_friends route', () => {
     // TODO: when have backend add users to test database
 
-    it('should respond with HTTP 200 and an object in the response body', done => {
-        request
-            .execute(app)
-            .get('/query_friends?userId=101')
+    it('should respond with HTTP 200 and an object in the response body', async function () {
+        chai.request(app)
+            .get('/query_friends?userId=3')
             .end((err, res) => {
                 expect(res).to.have.status(200);
                 // expect(res.body.friends).to.have.lengthOf(5); // adjust as per actual response shape
@@ -21,9 +21,8 @@ describe('GET request to /query_friends route', () => {
 
 
 
-    it('it should respond with an HTTP 400 status: no user id', done => {
-        request
-            .execute(app)
+    it('it should respond with an HTTP 400 status: no user id', async function () {
+        chai.request(app)
             .get('/query_friends?user_id=')
             .end((err, res) => {
                 expect(res).to.have.status(400);
