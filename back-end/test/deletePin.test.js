fa91b32f9628b02bb38d759984cf4321a1eb3cee
abbyibarra <109445721+abbyibarra@users.noreply.github.com>
2025-04-15 17:25:26 -0400

    updated delete pin test to use supertest

--- back-end/test/deletePin.test.js
+++ back-end/test/deletePin.test.js
@@ -1,34 +1,31 @@
-import { expect, use } from 'chai';
-import chaiHttp from 'chai-http';
+import supertest from 'supertest';
+import { assert } from 'assert';
 import app from '../app.js';
 
-const chai = use(chaiHttp);
-
-describe('DELETE /delete_pin', function() {
-
-  it('should return 400 if pinId is not provided', async function() {
-    const res = await chai.request(app)
+describe('DELETE /delete_pin', function () {
+  it('should return 400 if pinId is not provided', async function () {
+    const res = await request (app)
       .delete('/delete_pin')
       .send({});
 
-    expect(res).to.have.status(400);
-    expect(res.body).to.have.property('error', 'pinId is required');
+    assert.strictEqual(res.status, 400);
+    assert.strictEqual(res.body.error, 'pinId is required');
   });
 
-  it('should return 500 if pin does not exist in Mockaroo data', async function() {
-    const res = await chai.request(app)
+  it('should return 500 if pin does not exist in database', async function () {
+    const res = await request (app)
       .delete('/delete_pin')
       .send({ pinId: 999999 });
 
-    expect(res).to.have.status(500);
-    expect(res.body).to.have.property('error', 'Failed to delete pin from Mockaroo data');
+    assert.strictEqual(res.status, 500);
+    assert.strictEqual(res.body.error, 'Failed to delete pin from database');
   });
 
-  it('should return 200 if pin is found and "deleted"', async function() {
-    const res = await chai.request(app)
+  it('should return 200 if pin is found and "deleted"', async function () {
+    const res = await request (app)
       .delete('/delete_pin')
-      .send({ pinId: 85 });
+      .send({ pinId: 1 });
 
-    expect(res).to.have.status(200);
-  });
+    assert.strictEqual(res.status, 200);
+  })
 });
