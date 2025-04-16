ade044d3d7a1abcdede896f1bbabfc0381ea4a18
swdomo <swd7169@nyu.edu>
2025-04-15 23:08:19 -0400

    add profile pic to location Item

--- front-end/src/components/LocationItem.js
+++ front-end/src/components/LocationItem.js
@@ -28,7 +28,11 @@ const LocationItem = ({ location, removeLocation }) => {
                 <div className="flex justify-between w-full py-3.5">
                     <div>
                         <div className="flex gap-3">
-                            <PinIcon />
+                            <img
+                                src={locationUser.profilePicture}
+                                alt={`${locationUser.firstName} ${locationUser.lastName}`}
+                                className="rounded-full mr-3"
+                            />
                             <span className="text-lg">{locationUser.firstName}</span>
                         </div>
                     </div>
