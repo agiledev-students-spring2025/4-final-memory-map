e82933ee30c2adcc116f44feb6e15d940bd91fe8
Yinqi596 <yw6445@nyu.edu>
2025-04-28 15:29:10 -0400

    Update app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,6 +12,7 @@ import NoPage from './pages/NoPage';
 import NewLocation from './pages/NewLocation';
 import AddFriend from './pages/AddFriend';
 import FriendRequests from './pages/FriendRequests';
+import UserProfile from './pages/UserProfile';
 
 function App() {
   const location = useLocation();
@@ -36,6 +37,7 @@ function App() {
             <Route path="/new-location" element={<NewLocation />} />
             <Route path="/add-friend" element={<AddFriend />} />
             <Route path="/friend-requests" element={<FriendRequests />} />
+            <Route path="/user-profile" element={<UserProfile />} />
             <Route path="*" element={<NoPage />} />
           </Routes>
         </div>
