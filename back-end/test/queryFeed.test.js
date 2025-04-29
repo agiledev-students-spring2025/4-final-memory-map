37188df0176b29d15a052820e934789ad03dba86
abbyibarra <109445721+abbyibarra@users.noreply.github.com>
2025-04-29 12:40:00 -0400

    create user and pins

--- back-end/test/queryFeed.test.js
+++ back-end/test/queryFeed.test.js
@@ -21,23 +21,86 @@ let user;
 let friend;
 
 describe('GET /query_feed', function () {
-  let app;
-  let authStub;
+  this.timeout(10000);
 
-  beforeEach(function () {
-    app = express();
-    app.use(express.json());
-
-    authStub = sinon.stub(authModule, 'authenticate').callsFake((req, res, next) => {
-      req.user = { _id: 'fakeUserId' };
-      next();
-    });
-
-    app.use('/', route);
+  before(async function () {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri);
   });
 
-  afterEach(function () {
-    sinon.restore();
+  after(async function () {
+    await mongoose.disconnect();
+    if (mongoServer) await mongoServer.stop();
+  });
+
+  beforeEach(async function () {
+    await User.deleteMany({});
+    await Pin.deleteMany({});
+
+    // test user
+    user = await User.create({
+      username: 'testuser',
+      email: 'test@example.com',
+      password: 'Test@password123'
+    });
+
+    token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    // friend user
+    friend = await User.create({
+      username: 'frienduser',
+      email: 'friend@example.com',
+      password: 'Friend@password123'
+    });
+
+    // add friend to user's friends list
+    user.friends.push(friend._id);
+    await user.save();
+
+    // pin by the user (own pin)
+    await Pin.create({
+      title: 'Own Pin',
+      description: 'My private pin',
+      location: { type: 'Point', coordinates: [-74.0060, 40.7128] },
+      locationName: 'Test Location',
+      imageUrl: 'http://example.com/own-pin.jpg',
+      visibility: 1,
+      author: user._id
+    });
+
+    // public pin by the friend
+    await Pin.create({
+      title: 'Friend Public Pin',
+      description: 'My public pin',
+      location: { type: 'Point', coordinates: [-73.935242, 40.730610] },
+      locationName: 'Friend Location',
+      imageUrl: 'http://example.com/friend-pin.jpg',
+      visibility: 3,
+      author: friend._id
+    });
+
+    // private pin by the friend (should not be visible)
+    await Pin.create({
+      title: 'Friend Private Pin',
+      description: 'Should not be visible',
+      location: { type: 'Point', coordinates: [-73.961452, 40.678178] },
+      locationName: 'Private Location',
+      imageUrl: 'http://example.com/private-pin.jpg',
+      visibility: 1,
+      author: friend._id
+    });
+
+    // friend-only pin by the friend
+    await Pin.create({
+      title: 'Friend Only Pin',
+      description: 'Visible to friends',
+      location: { type: 'Point', coordinates: [-73.950000, 40.650000] },
+      locationName: 'Friend Only Location',
+      imageUrl: 'http://example.com/friend-only-pin.jpg',
+      visibility: 2,
+      author: friend._id
+    });
   });
 
   it('should return 200 and an array of filtered pins', async function () {
